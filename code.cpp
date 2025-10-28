830a4f9f6e16efc5da2451cab11acb85731644f5
sahil kumar <kumarsahil0910@gmail.com>
2025-10-28 21:45:25 +0530

    exp 4 done

--- code.cpp
+++ code.cpp
@@ -2,14 +2,54 @@
 using namespace std;
 int main() {
     // Write C++ code here
-int num;
-cout << " number:";
-cin >>num;
-if (num >0) {
-    cout << "Number" <<  num <<"is positive."<<endl;
-}
-else {
-    cout <<"number" << num << "is negative." << endl ;
-}
+// int num;
+// cout << " number:";
+// cin >>num;
+// if (num >0) {
+//     cout << "Number" <<  num <<"is positive."<<endl;
+// }
+// else {
+//     cout <<"number" << num << "is negative." << endl ;
+// }
+int age;
+int age=25;
+cout << age << endl;
+char grade='a';
+cout << grade << endl;
+float pi=3.14;
+cout<<pi<<endl;
+char grade='a';
+float value=grade;
+cout<<value<<endl;
+double price=100.9;
+int newprice=int price;
+cout<<newprice<<endl;
+int age;
+cout<<"enter the age:";
+cin>>age;
+cout<<"your age is:"<<age<<endl;
+int a= ,b;
+cout <<"enter a:";
+cin>>a;
+cout <<"enter b:";
+ int age;
+    cout<<"enter the age:";
+    cin >>age;
+    if(age>=18){
+        cout<<"eligible for voting\n";
+    }else{cout<<"not eligible for voting";}
+    int n;
+    cout <<"enter the value of n:";
+    cin >>n;
+    if(n/2==0){
+        cout<<"n is even";
+    }else{cout<<"n is odd";}
+
+char ch;
+cout<<"enter char:";
+cin>>ch;
+if(ch>='a'&& ch<='z'){
+    cout<<"lower case";
+}else{cout<<"upper case";}
     return 0;
 }
